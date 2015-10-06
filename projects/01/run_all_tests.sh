5f362a51207e8fa251af1146ed5249ba394a3005
Maksim Yegorov <findmaksim@gmail.com>
2015-10-05 22:25:45 -0400

    modified batch test

--- projects/01/run_all_tests.sh
+++ projects/01/run_all_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # This script tests all projects/01/*.hdl chips in batch
 # Author: Maksim Yegorov <findmaksim@gmail.com>
-# Last edit: 2015-10-05 Mon 10:04 PM
+# Last edit: 2015-10-05 Mon 10:25 PM
 
 for filename in ./*.hdl; do
-    basename=$filename
-    basename=${basename%.*}
-    printf "\n"; echo $basename 
-    ../../tools/HardwareSimulator.sh $basename.tst
+    file=${filename##*/}
+    base=${file%.*}
+    printf "\n"; echo $base.hdl
+    ../../tools/HardwareSimulator.sh $base.tst
 done
