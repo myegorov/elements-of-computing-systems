ab35af576bc929df48963b9a9285c68970a12871
Maksim Yegorov <findmaksim@gmail.com>
2015-11-19 01:47:53 -0500

    added multiplication script

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,28 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+    @R2
+    M = 0     // initialize M[R2] = 0
+
+    @R0     // check if M[R0] == 0
+    D = M
+    @END
+    D; JEQ   // no need to iterate if one of factors == 0
+
+(LOOP)      // while (R0 > 0)
+    @R1
+    D = M     // D = M[R1]
+    @R2
+    M = D + M   // M[R2] = M[R2] + M[R1]
+
+    @R0     // decrement R0
+    M = M - 1
+
+    D = M     // check if R0 > 0
+    @LOOP
+    D; JGT   // goto LOOP
+
+(END)
+    @END
+    0; JMP   // infinite loop
